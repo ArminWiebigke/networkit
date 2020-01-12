6e5448f2ec1b9197f828f49d4c30b4ae4f77845b
Armin Wiebigke <armin.wiebigke@gmail.com>
2020-01-12 15:59:18 +0100

    Return result of CommunityDetectionAlgorithm as a const reference

--- networkit/cpp/community/CommunityDetectionAlgorithm.cpp
+++ networkit/cpp/community/CommunityDetectionAlgorithm.cpp
@@ -1,5 +1,5 @@
 /*
- * Clusterer.cpp
+ * CommunityDetectionAlgorithm.cpp
  *
  *  Created on: 30.10.2012
  *      Author: Christian Staudt (christian.staudt@kit.edu)
@@ -9,18 +9,19 @@
 
 namespace NetworKit {
 
-CommunityDetectionAlgorithm::CommunityDetectionAlgorithm(const Graph& G) : Algorithm(), G(G), result(0) {
+CommunityDetectionAlgorithm::CommunityDetectionAlgorithm(const Graph &G) : Algorithm(), G(G), result(0) {
     // currently our community detection methods are not defined on directed graphs
     if (G.isDirected()) {
         throw std::runtime_error("This community detection method is undefined on directed graphs");
     }
 }
 
-CommunityDetectionAlgorithm::CommunityDetectionAlgorithm(const Graph& G, const Partition baseClustering) : Algorithm(), G(G), result(baseClustering) {
+CommunityDetectionAlgorithm::CommunityDetectionAlgorithm(const Graph &G, const Partition &baseClustering)
+        : Algorithm(), G(G), result(baseClustering) {
 }
 
-Partition CommunityDetectionAlgorithm::getPartition() {
-    if(!hasRun) {
+const Partition &CommunityDetectionAlgorithm::getPartition() const {
+    if (!hasRun) {
         throw std::runtime_error("Call run()-function first.");
     }
     return result;
